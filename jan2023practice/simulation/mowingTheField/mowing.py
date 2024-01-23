45c0511e76f4d1b13fe8bfb5333bd5b7a9683e79
Ray <raytang11792@gmail.com>
2024-01-23 18:45:04 -0500

    mowing lawn prblesm

--- jan2023practice/simulation/mowingTheField/mowing.py
+++ jan2023practice/simulation/mowingTheField/mowing.py
@@ -1,18 +1,53 @@
-with open('jan2023practice/simulation/mowingTheField/mowing.in', 'r') as infile:
+with open('mowing.in', 'r') as infile:
     n = int(infile.readline())
     data = []
     for _ in range(n):
         temp = infile.readline().split()
         data.append([temp[0], int(temp[1])])
 
+
 cellVisits = [] # log visited cells and times
 fjPosition = [0, 0]
+time = 0
+cellVisits.append([0, 0, 0]) # first mowed cell is when he starts, [x, y, time]
 for move in data: #move = ['N', 4]
     if move[0] == 'N':
-        fjPosition[1] += move[1]
+        # fjPosition[1] += move[1]
+        for x in range(move[1]): # make farmer john mow cells one at a time
+            fjPosition[1] += 1
+            time += 1
+            cellVisits.append([fjPosition[0], fjPosition[1], time])
     if move[0] == 'E':
-        fjPosition[0] += move[1]
+        for x in range(move[1]): # make farmer john mow cells one at a time
+            fjPosition[0] += 1
+            time += 1
+            cellVisits.append([fjPosition[0], fjPosition[1], time])
     if move[0] == 'S':
-        fjPosition[1] -= move[1]
+        for x in range(move[1]): # make farmer john mow cells one at a time
+            fjPosition[1] -= 1
+            time += 1
+            cellVisits.append([fjPosition[0], fjPosition[1], time])
     if move[0] == 'W':
-        fjPosition[0] -= move[1]
+        for x in range(move[1]): # make farmer john mow cells one at a time
+            fjPosition[0] -= 1
+            time += 1
+            cellVisits.append([fjPosition[0], fjPosition[1], time])
+
+# now iterasthe through cell visitss
+# if two coords are equal, FJ revisits a cell
+# append diffenece in time to a list, find min of list
+timeDifferences = [] #amount of time it takes him to revisit cells
+for coord in cellVisits:
+    for comparedCoord in cellVisits:
+        if coord == comparedCoord:
+            continue
+        if (coord[0] == comparedCoord[0]) and (coord[1] == comparedCoord[1]):
+            # if coords match (he revisites)
+            timeDifferences.append(max(coord[2], comparedCoord[2]) - min(coord[2], comparedCoord[2]))
+
+with open('mowing.out', 'w') as outfile:
+    if len(timeDifferences) == 0:
+        outfile.write("-1")
+        quit()
+    else:
+        outfile.write(str(min(timeDifferences)))
